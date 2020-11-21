f73b87798de26629b11e52c787616aabc6c9b5dc
suvambasak <suvambasak@gmail.com>
2020-11-21 18:42:08 +0530

    sjf

--- cpu/SJF.py
+++ cpu/SJF.py
@@ -19,11 +19,11 @@ def get_index(pid_q,at_q,bt_q):
         return pid_q[index]
 
 
-def schedule(p_id,arrival_time,burst_time):
-    print (p_id)
-    print (arrival_time)
-    print (burst_time)
-    completion_time = [0]*(len(p_id)+1)
+def schedule(p_id,arrival_time,burst_time,completion_time):
+    # print (p_id)
+    # print (arrival_time)
+    # print (burst_time)
+    
     current_time=0
     
 
@@ -38,14 +38,15 @@ def schedule(p_id,arrival_time,burst_time):
                 at_q.append(arrival_time[i])
                 bt_q.append(burst_time[i])
 
-        print(pid_q)
-        print(at_q)
-        print(bt_q)
+        # print(pid_q)
+        # print(at_q)
+        # print(bt_q)
 
         if len(pid_q)==1:
             index = p_id.index(pid_q[0])
             current_time+=burst_time[index]
             completion_time[p_id[index]]=current_time
+            print ('Processing PID ',pid_q[0])
             del p_id[index]
             del arrival_time[index]
             del burst_time[index]
@@ -53,18 +54,24 @@ def schedule(p_id,arrival_time,burst_time):
             index = p_id.index(get_index(pid_q,at_q,bt_q))
             current_time+=burst_time[index]
             completion_time[p_id[index]]=current_time
+            print ('Processing PID ',pid_q[index])
             del p_id[index]
             del arrival_time[index]
             del burst_time[index]
         else:
             current_time+=1
 
-        print (completion_time)
-        print (p_id)
-        print (arrival_time)
-        print (burst_time)
+        # print (completion_time)
+        # print (p_id)
+        # print (arrival_time)
+        # print (burst_time)
 
 
+def show_details(p_id,arrival_time,burst_time,completion_time):
+    print("PID\tAT\tCPU\tCOM\tTAT\tWT")
+    for i in range(0,len(p_id)):
+        print (p_id[i],'\t',arrival_time[i],'\t',burst_time[i],'\t',completion_time[p_id[i]],'\t',(completion_time[p_id[i]]-arrival_time[i]),'\t',(completion_time[p_id[i]]-arrival_time[i]-burst_time[i]))
+        
 
 
 if __name__ == '__main__':
@@ -79,4 +86,6 @@ if __name__ == '__main__':
     # p_id = [1, 2, 3, 4, 5,6]
     # arrival_time = [6, 3, 5, 1, 4,2]
     # burst_time = [8, 3, 6, 2, 4,2]
-    schedule(p_id[:],arrival_time[:],burst_time[:])
+    completion_time = [0]*(len(p_id)+1)
+    schedule(p_id[:],arrival_time[:],burst_time[:],completion_time)
+    show_details(p_id,arrival_time,burst_time,completion_time)
