e893b7e6ae98ebc8934db216ad58da776c04050d
suvambasak <suvambasak@gmail.com>
2020-11-21 21:12:03 +0530

    LJF

--- cpu/LJF.py
+++ cpu/LJF.py
@@ -1,10 +1,10 @@
-def get_index(pid_q,at_q,pr_q):
-    if pr_q.count(max(pr_q))>1:
-        m=max(pr_q)
+def get_index(pid_q,at_q,bt_q):
+    if bt_q.count(max(bt_q))>1:
+        m=max(bt_q)
         min_l_at=[]
         min_l_pid=[]
-        for i in range(0,len(pr_q)):
-            if m == pr_q[i]:
+        for i in range(0,len(bt_q)):
+            if m == bt_q[i]:
                 min_l_at.append(at_q[i])
                 min_l_pid.append(pid_q[i])
 
@@ -15,11 +15,11 @@ def get_index(pid_q,at_q,pr_q):
             return pid_q[index]
         
     else:
-        index = pr_q.index(max(pr_q)) 
+        index = bt_q.index(max(bt_q)) 
         return pid_q[index]
 
 
-def schedule(p_id,arrival_time,burst_time,priority,completion_time):
+def schedule(p_id,arrival_time,burst_time,completion_time):
     # print (p_id)
     # print (arrival_time)
     # print (burst_time)
@@ -30,13 +30,13 @@ def schedule(p_id,arrival_time,burst_time,priority,completion_time):
     while len(p_id):
         pid_q=[]
         at_q=[]
-        pr_q=[]
+        bt_q=[]
         
         for i in range(0,len(p_id)):
             if arrival_time[i] <= current_time:
                 pid_q.append(p_id[i])
                 at_q.append(arrival_time[i])
-                pr_q.append(priority[i])
+                bt_q.append(burst_time[i])
 
         # print(pid_q)
         # print(at_q)
@@ -50,16 +50,14 @@ def schedule(p_id,arrival_time,burst_time,priority,completion_time):
             del p_id[index]
             del arrival_time[index]
             del burst_time[index]
-            del priority[index]
         elif len(pid_q)>1:
-            index = p_id.index(get_index(pid_q,at_q,pr_q))
+            index = p_id.index(get_index(pid_q,at_q,bt_q))
             current_time+=burst_time[index]
             completion_time[p_id[index]]=current_time
             print ('Processing PID ',pid_q[index])
             del p_id[index]
             del arrival_time[index]
             del burst_time[index]
-            del priority[index]
         else:
             current_time+=1
 
@@ -68,30 +66,26 @@ def schedule(p_id,arrival_time,burst_time,priority,completion_time):
         # print (arrival_time)
         # print (burst_time)
 
-def show_details(p_id,priority,arrival_time,burst_time,completion_time):
-    print("PID\tPRI\tAT\tCPU\tCOM\tTAT\tWT")
-    for i in range(0,len(p_id)):
-        print (p_id[i],'\t',priority[i],'\t',arrival_time[i],'\t',burst_time[i],'\t',completion_time[p_id[i]],'\t',(completion_time[p_id[i]]-arrival_time[i]),'\t',(completion_time[p_id[i]]-arrival_time[i]-burst_time[i]))
 
+def show_details(p_id,arrival_time,burst_time,completion_time):
+    print("PID\tAT\tCPU\tCOM\tTAT\tWT")
+    for i in range(0,len(p_id)):
+        print (p_id[i],'\t',arrival_time[i],'\t',burst_time[i],'\t',completion_time[p_id[i]],'\t',(completion_time[p_id[i]]-arrival_time[i]),'\t',(completion_time[p_id[i]]-arrival_time[i]-burst_time[i]))
+        
 
 
 if __name__ == '__main__':
     p_id = list(map(int,input('PROCESS IDs: ').split(' ')))
-    priority = list(map(int,input('PRIORITY: ').split(' ')))
     arrival_time = list(map(int,input('ARRIVAL TIMEs: ').split(' ')))
     burst_time = list(map(int,input('BURST TIMEs: ').split(' ')))
 
-    # priority = [3, 2, 4, 6, 7]
     # p_id = [1, 2, 3, 4, 5]
     # arrival_time = [0, 1, 2, 3, 4]
-    # burst_time = [2, 3, 6, 8, 9]
+    # burst_time = [2, 4, 6, 5, 8]
 
-    # print (priority)
-    # print (p_id)
-    # print (arrival_time)
-    # print (burst_time)
-
+    # p_id = [1, 2, 3, 4, 5,6]
+    # arrival_time = [6, 3, 5, 1, 4,2]
+    # burst_time = [8, 3, 6, 2, 4,2]
     completion_time = [0]*(len(p_id)+1)
-    schedule(p_id[:],arrival_time[:],burst_time[:],priority[:],completion_time)
-    # print (completion_time)
-    show_details(p_id,priority,arrival_time,burst_time,completion_time)
+    schedule(p_id[:],arrival_time[:],burst_time[:],completion_time)
+    show_details(p_id,arrival_time,burst_time,completion_time)
--- cpu/SJF.py
+++ cpu/SJF.py
@@ -75,13 +75,13 @@ def show_details(p_id,arrival_time,burst_time,completion_time):
 
 
 if __name__ == '__main__':
-    # p_id = list(map(int,input('PROCESS IDs: ').split(' ')))
-    # arrival_time = list(map(int,input('ARRIVAL TIMEs: ').split(' ')))
-    # burst_time = list(map(int,input('BURST TIMEs: ').split(' ')))
+    p_id = list(map(int,input('PROCESS IDs: ').split(' ')))
+    arrival_time = list(map(int,input('ARRIVAL TIMEs: ').split(' ')))
+    burst_time = list(map(int,input('BURST TIMEs: ').split(' ')))
 
-    p_id = [1, 2, 3, 4, 5]
-    arrival_time = [0, 1, 2, 3, 4]
-    burst_time = [7, 5, 6, 2, 4]
+    # p_id = [1, 2, 3, 4, 5]
+    # arrival_time = [0, 1, 2, 3, 4]
+    # burst_time = [7, 5, 6, 2, 4]
 
     # p_id = [1, 2, 3, 4, 5,6]
     # arrival_time = [6, 3, 5, 1, 4,2]
