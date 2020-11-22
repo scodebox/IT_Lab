78e319e1cf8750d3c0115b4c0d4d474cd8adbd20
suvambasak <suvambasak@gmail.com>
2020-11-22 23:50:38 +0530

    file comments

--- file/file_cmp.py
+++ file/file_cmp.py
@@ -1,8 +1,8 @@
 # function for comparing char by char.
 def error_check(line1, line2):
-    error_count=0   # var for counting errors.
+    error_count = 0   # var for counting errors.
     error_index = []    # list for keep recording error index.
-    
+
     # striping out space or tab from start/end.
     line1 = line1.strip()
     line2 = line2.strip()
@@ -10,11 +10,11 @@ def error_check(line1, line2):
     # checking length of two lines.
     if len(line1) == len(line2):
         # char to char compare and record index & increase error count if mismatch found.
-        for i in range(0,len(line2)):
-            if line1[i]!=line2[i]:
-                error_count+=1
+        for i in range(0, len(line2)):
+            if line1[i] != line2[i]:
+                error_count += 1
                 error_index.append(i)
-        print('Error index : ',error_index)
+        print('Error index : ', error_index)
         print('Error count : ', error_count)
         print()
     else:
@@ -22,8 +22,8 @@ def error_check(line1, line2):
 
 
 # opening two files.
-file1 = open('file1.txt','r')
-file2 = open('file2.txt','r')
+file1 = open('file1.txt', 'r')
+file2 = open('file2.txt', 'r')
 
 # line number counter.
 line_number = 0
@@ -39,15 +39,15 @@ while True:
         break
 
     # line count.
-    line_number+=1
+    line_number += 1
 
     # Checking lines are exactly same.
     if line1 == line2:
-        print("Line number ",line_number," MATCHED ")
+        print("Line number ", line_number, " MATCHED ")
     else:
-        print("\nLine number ",line_number," ERROR")
+        print("\nLine number ", line_number, " ERROR")
         # calling error check fucntion to compares char by char.
-        error_check(line1,line2)
+        error_check(line1, line2)
     # print (1,line1.strip())
     # print (2,line2.strip())
 
