b4c10b2f4ef19f71027743e2574fa01000690ed8
suvambasak <suvambasak@gmail.com>
2020-11-23 00:19:52 +0530

    sync comments

--- sync/peterson.py
+++ sync/peterson.py
@@ -1,23 +1,31 @@
 import threading
 import time
 
-
-turn=0
-flag = [False,False]
+# Initialization
+turn = 0
+flag = [False, False]
 
 
 def p(n):
-	while True:
-		flag[n] = True
-		turn=1-n
-		while (turn==(1-n) and (flag[1-n]==True)):
-			print ('Process ',n,' waiting.')
-		print (print ('Process ',n,' inside critical section.'))
-		flag[n]=False
-		time.sleep(1)
+    while True:
+        # Setting flag true and giving turn to other process.
+        flag[n] = True
+        turn = 1-n
+
+        # Checking other process trun and flag.
+        while (turn == (1-n) and (flag[1-n] == True)):
+            print('Process ', n, ' waiting.')
+
+        print(print('Process ', n, ' inside Critical Section.'))
+        # Setting flag false.
+        flag[n] = False
+        time.sleep(1)
 
 
+# Creating thread for process 1.
 t1 = threading.Thread(target=p, args=(0,))
 t1.start()
+
+# Creating thread for process 2.
 t2 = threading.Thread(target=p, args=(1,))
 t2.start()
--- sync/semaphore.py
+++ sync/semaphore.py
@@ -1,26 +1,36 @@
 import threading
 
+# Initialization of semaphore for two process.
 semaphore = threading.Semaphore(2)
 
+
 def fact(n):
-	semaphore.acquire()
-	print ('fun : ',n,': semaphore value : ',semaphore._value)
-	print ('fun : ',n,': semaphore name : ',threading.current_thread().name)
-	f=1
-	name=n
-	while (n>0):
-		f=f*n
-		print ('fact: ',name)
-		n-=1
+    # acquiring the semaphore.
+    semaphore.acquire()
+    # Showing the value of semaphore.
+    print('fun : ', n, ': semaphore value : ', semaphore._value)
+    # Showing the name of the thread.
+    print('fun : ', n, ': semaphore name : ', threading.current_thread().name)
 
-	print('fun : ',name,'complete')
-	semaphore.release()
+    f = 1
+    name = n
+    while (n > 0):
+        f = f*n
+        print('fact: ', name)
+        n -= 1
+
+    print('fun : ', name, 'complete')
+    # Releasing the semaphore.
+    semaphore.release()
 
 
+# Creating three thread to check semaphore.
 t1 = threading.Thread(target=fact, args=(10,))
 t2 = threading.Thread(target=fact, args=(8,))
 t3 = threading.Thread(target=fact, args=(7,))
+# Starting all the thread.
 t1.start()
 t2.start()
 t3.start()
-print ('main Thread: ',threading.main_thread())
+# Showing main thread details.
+print('main Thread: ', threading.main_thread())
